9dd447b50ae3cf1d7407fdca18acc1641c317bf9
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-08 20:17:01 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+var playlist={
+  RHCP: "Aeroplane"
+}
+
+function updatePlaylist(playlist, {artist, song}){
+  Object.assign(playlist, artist: song)
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+}
