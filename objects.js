d60108b69284e094db7a2412c77166bdc1767ecc
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-08 20:39:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,5 @@ var playlist={
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  playlist=Object.assign({}, playlist, { [artistName]: songTitle })
-  return playlist
+  playlist[artistName]=songTitle;
 }
