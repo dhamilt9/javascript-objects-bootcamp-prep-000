5e366632e8f0be0f6722f373c5f8e850ad0d34fd
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-08 20:46:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,10 +2,11 @@ var playlist={
   Test: 'Aeroplane'
 }
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName]=songTitle;
-}
 
 function removeFromPlaylist(a, b){
   delete a.b;
+}
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName]=songTitle;
 }
