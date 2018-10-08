44716429569ab1e33029fe59bbe67edf24b57bc7
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-08 20:22:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,6 +2,6 @@ var playlist={
   RHCP: "Aeroplane"
 }
 
-function updatePlaylist(playlist, artist, song){
-  return Object.assign(playlist, {artist: song})
+function updatePlaylist(playlist, artistName, songTitle){
+  return Object.assign({}, playlist, {artist: song})
 }
