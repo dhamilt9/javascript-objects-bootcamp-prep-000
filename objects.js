ff3963a7af78e56fc4453b34442036fa52def9b2
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-08 20:17:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,7 +5,3 @@ var playlist={
 function updatePlaylist(playlist, {artist, song}){
   Object.assign(playlist, artist: song)
 }
-
-function removeFromPlaylist(playlist, artist){
-  delete playlist.artist
-}
